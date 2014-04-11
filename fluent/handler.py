c04cf227ab87d655393191011078417970bb1ba9
John Crawford <jrcrawfo@ncsu.edu>
2014-04-11 17:02:18 +0000

    Added exception log formatting

--- fluent/handler.py
+++ fluent/handler.py
@@ -1,34 +1,45 @@
-# -*- coding: utf-8 -*-
-
 import logging
+import os
+import sys
+import msgpack
 import socket
+import threading
+import traceback
 
 try:
-    import simplejson as json
-except ImportError:
     import json
+except ImportError:
+    import simplejson as json
 
 from fluent import sender
-
 
 class FluentRecordFormatter(object):
     def __init__(self):
         self.hostname = socket.gethostname()
 
-    def format(self, record):
-        data = {'sys_host': self.hostname,
-                'sys_name': record.name,
-                'sys_module': record.module,
-                # 'sys_lineno': record.lineno,
-                # 'sys_levelno': record.levelno,
-                # 'sys_levelname': record.levelname,
-                # 'sys_filename': record.filename,
-                # 'sys_funcname': record.funcName,
-                # 'sys_exc_info': record.exc_info,
-                }
-        # if 'sys_exc_info' in data and data['sys_exc_info']:
-        #    data['sys_exc_info'] = self.formatException(data['sys_exc_info'])
+    def formatException(self, exceptionInfo):
+      eData = {}
+      eData['type'] = str(exceptionInfo[0])
+      eData['details'] = str(exceptionInfo[1])
+      eData['trace'] = traceback.format_exc()
+      return eData
 
+    def format(self, record):
+        data = {
+          'sys_host' : self.hostname,
+          'sys_name' : record.name,
+          'sys_module' : record.module,
+          # 'sys_lineno' : record.lineno,
+          # 'sys_levelno' : record.levelno,
+          'sys_levelname' : record.levelname,
+          # 'sys_filename' : record.filename,
+          # 'sys_funcname' : record.funcName,
+          'sys_exc_info' : record.exc_info,
+        }
+        if 'sys_exc_info' in data and data['sys_exc_info']:
+           data['sys_exc_info'] = self.formatException(data['sys_exc_info'])
+        else:
+           del data['sys_exc_info']
         self._structuring(data, record.msg)
         return data
 
@@ -38,41 +49,36 @@ class FluentRecordFormatter(object):
         elif isinstance(msg, str):
             try:
                 self._add_dic(data, json.loads(str(msg)))
-            except (ValueError, json.JSONDecodeError):
+            except:
                 pass
 
-    @staticmethod
-    def _add_dic(data, dic):
-        for key, value in dic.items():
-            if isinstance(key, basestring):
-                data[str(key)] = value
-
+    def _add_dic(self, data, dic):
+        for k, v in dic.items():
+            if isinstance(k, str) or isinstance(k, unicode):
+                data[str(k)] = v
 
 class FluentHandler(logging.Handler):
     '''
     Logging Handler for fluent.
     '''
     def __init__(self,
-                 tag,
-                 host='localhost',
-                 port=24224,
-                 timeout=3.0,
-                 verbose=False):
+           tag,
+           host='localhost',
+           port=24224,
+           timeout=3.0,
+           verbose=False):
 
         self.tag = tag
         self.sender = sender.FluentSender(tag,
                                           host=host, port=port,
                                           timeout=timeout, verbose=verbose)
+        self.fmt = FluentRecordFormatter()
         logging.Handler.__init__(self)
 
     def emit(self, record):
-        data = self.format(record)
+	if record.levelno < self.level: return
+        data = self.fmt.format(record)
         self.sender.emit(None, data)
 
-    def close(self):
-        self.acquire()
-        try:
-            self.sender._close()
-            logging.Handler.close(self)
-        finally:
-            self.release()
+    def _close(self):
+        self.sender._close()
